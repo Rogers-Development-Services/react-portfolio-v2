588018dfde60a2ba0580296b272ceaa3f1522a52
Matthew Shane Rogers <matthew.shane.rogers@gmail.com>
2021-01-06 16:28:25 -0800

    Refactored Contact page to remove inline styles and added thank you message when user submits contact information.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import "../assets/styles/Contact.css";
+import submitForm from "../utils/form-submission-handler";
 import { Textarea, Icon, TextInput, Button, Row, Col } from 'react-materialize';
 
-
 function Contact() {
+
+  const [submit, setSubmit] = useState("none");
+
+  function handleSubmit() {
+    setSubmit("block");
+    submitForm();
+  };
+
   return (
-    <div 
-    className="container"
-    style={{fontFamily: "nunito"}}>
+    <div className="container">
       <h2>Contact Us!</h2>
       <Row>
-        <Col
-          style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}
-          s={12} l={12}>
-          <p>We would <em>love</em> to hear from you! </p>
+        <Col className="contact-intro"
+          s={12} m={12} l={12}
+        >
+          <p>
+            We would <em>love</em> to hear from you!
+          </p>
+          <p>
+            Connect with us with the form below, or at <a className="about-us-links" href="tel:9169524662">916-952-4662</a> &#38; <a className="about-us-links" href="mailto:matthew.shane.rogers@gmail.com">matthew.shane.rogers@gmail.com</a>.
+          </p>
         </Col>
       </Row>
 
-      {/* START HERE */}
-      {/* Style The Contact Form How Ever You Prefer */}
       <Row>
         <Col s={12} l={12}>
           <form
@@ -27,7 +36,6 @@ function Contact() {
             data-email="matthew.shane.rogers@gmail.com"
             action="https://script.google.com/macros/s/AKfycbz0lCgoEhYNK-aUDuMl-cy1q2z1EdI4h4BDNwpd/exec"
           >
-            {/* change the form action to your script url */}
 
             <Row>
               <Col s={6} l={6}>
@@ -37,7 +45,6 @@ function Contact() {
                   icon="person"
                   id="client-name"
                   label="First Name"
-                  placeholder="Matthew"
                   s={12} l={12}
                 />
               </Col>
@@ -47,7 +54,6 @@ function Contact() {
                   name="name"
                   id="client-name"
                   label="Last Name"
-                  placeholder="Rogers"
                   s={12} l={12}
                 />
               </Col>
@@ -61,7 +67,6 @@ function Contact() {
                   name="message"
                   id="client-message"
                   label="Message"
-                  placeholder="How can we help you?"
                   s={12} l={12}
                 />
               </Col>
@@ -76,18 +81,17 @@ function Contact() {
                   icon="email"
                   id="client-email"
                   label="Email"
-                  placeholder="your.name@email.com"
                   validate
                   s={12} l={12}
                 />
               </Col>
             </Row>
             <Row>
-              <Col style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}
+              <Col className="submit-col"
                 s={12} l={12}>
                 <Button
-                  // className="submit-button"
-                  style={{ margin: "1rem" }}
+                  className="submit-button"
+                  onClick={handleSubmit}
                   node="button"
                   type="submit"
                   waves="light"
@@ -97,22 +101,13 @@ function Contact() {
               </Col>
             </Row>
 
-
-            {/* <fieldset className="pure-group honeypot-field" style={{ display: "none" }}>
-          <label for="honeypot">To help avoid spam, utilize a Honeypot technique with a hidden text field; must be empty to submit the form! Otherwise, we assume the user is a spam bot.</label>
-          <input id="honeypot" type="text" name="honeypot" value="" />
-        </fieldset> */}
-
-            {/* Customise the Thankyou Message People See when they submit the form: */}
-            <div style={{ display: "none" }} className="thankyou_message">
-              <h2><em>Thanks</em> for contacting us! We will get back to you soon!</h2>
+            <div style={{ display: submit }} className="thankyou-message">
+              <h4><em>Thank you</em> for contacting us! We will get back to you soon!</h4>
             </div>
 
           </form>
         </Col>
       </Row>
-      {/* Submit the Form to Google Using "AJAX" */}
-      <script data-cfasync="false" src="form-submission-handler.js"></script>
     </div>
   );
 }
